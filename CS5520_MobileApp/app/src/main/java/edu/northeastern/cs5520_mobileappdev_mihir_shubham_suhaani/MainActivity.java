fde2bf38dee6500614b09ea178975768af4eb527
Suhaani Agarwal <64849382+agarwalsuhaani@users.noreply.github.com>
2022-10-21 17:17:43 -0400

    Initial Project Setup

---
+++ CS5520_MobileApp/app/src/main/java/edu/northeastern/cs5520_mobileappdev_mihir_shubham_suhaani/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.cs5520_mobileappdev_mihir_shubham_suhaani;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ CS5520_MobileApp/app/src/test/java/edu/northeastern/cs5520_mobileappdev_mihir_shubham_suhaani/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package edu.northeastern.cs5520_mobileappdev_mihir_shubham_suhaani;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
