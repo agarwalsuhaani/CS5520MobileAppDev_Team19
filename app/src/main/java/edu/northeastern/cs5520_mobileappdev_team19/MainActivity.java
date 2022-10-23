afe28b168e8e8e8cb81939961218d9e33018df3f
Suhaani Agarwal <64849382+agarwalsuhaani@users.noreply.github.com>
2022-10-23 18:37:02 -0400

    Search bar added in GameList

--- app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/MainActivity.java
+++ app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/MainActivity.java
@@ -2,13 +2,35 @@ package edu.northeastern.cs5520_mobileappdev_team19;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_AtYourService, btn_firebase, btn_group;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void atYourService(View v) {
+        btn_AtYourService = findViewById(R.id.btn_AtYourService);
+        btn_AtYourService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, GameListActivity.class));
+            }
+        });
+    }
+
+    public void firebase(View v) {
+        btn_firebase = findViewById(R.id.btn_Firebase);
+    }
+
+    public void groupProject(View v) {
+        btn_group = findViewById(R.id.btn_group);
     }
 }
