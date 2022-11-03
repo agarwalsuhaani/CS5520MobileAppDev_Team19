5f6e4fb1eb55efe2ca84943d09b2f56c4f65d043
sghosalkar <shubhamghosalkar.sg@gmail.com>
2022-11-02 22:55:17 -0700

    Added login dialog and persist logged-in user in shared preferences

--- app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/UserListActivity.java
+++ app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/UserListActivity.java
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 
 import java.util.ArrayList;
@@ -21,6 +30,8 @@ public class UserListActivity extends AppCompatActivity {
     private UserViewAdapter userViewAdapter;
     private RecyclerView userRecyclerView;
     private UserService userService;
+    private User loggedInUser;
+    private static final String LOGGED_IN_USER_ID = "LOGGED_IN_USER_ID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +42,79 @@ public class UserListActivity extends AppCompatActivity {
         userRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         userViewAdapter = new UserViewAdapter(users, this);
         userRecyclerView.setAdapter(userViewAdapter);
+        userService = new UserService();
 
-        userService = new UserService(userViewAdapter);
+        initialize();
+    }
+
+    private void initialize() {
+        SharedPreferences preferences = this.getPreferences(Context.MODE_PRIVATE);
+        String loggedInUserId = preferences.getString(LOGGED_IN_USER_ID, null);
+        if (loggedInUserId != null) {
+            userService.getUser(loggedInUserId, user -> {
+                if (user != null) {
+                    setLoggedInUserAndLoadRecipients(user);
+                } else {
+                    showLoginDialog();
+                }
+            }, exception -> {
+                Log.e("firebase", "Error getting user", exception);
+                showLoginDialog();
+            });
+        } else {
+            showLoginDialog();
+        }
+    }
+
+    private void setLoggedInUserAndLoadRecipients(User user) {
+        loggedInUser = user;
+        loadRecipients(loggedInUser);
+    }
+
+    private void loadRecipients(User loggedInUser) {
+        userService.fetchUsers(userViewAdapter, loggedInUser);
         ProgressBar spinner = findViewById(R.id.progress_bar_user_list);
         spinner.setVisibility(View.GONE);
+    }
+
+    private void storeInPreferences(String key, String value) {
+        SharedPreferences preferences = this.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    private void showLoginDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Login using username");
+        View userLoginView = LayoutInflater.from(getBaseContext()).inflate(R.layout.user_login, null);
+        final EditText usernameEditText = userLoginView.findViewById(R.id.login_username);
+        builder.setView(userLoginView);
+
+        builder.setPositiveButton("Submit", (dialog, which) -> {
+            loggedInUser = new User(usernameEditText.getText().toString());
+            userService.registerUser(loggedInUser);
+            storeInPreferences(LOGGED_IN_USER_ID, loggedInUser.getId());
+            loadRecipients(loggedInUser);
+        });
+
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(false);
+        final TextWatcher watcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(!TextUtils.isEmpty(usernameEditText.getText().toString()));
+            }
+        };
+
+        usernameEditText.addTextChangedListener(watcher);
+        usernameEditText.addTextChangedListener(watcher);
     }
 }
--- app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/services/UserService.java
+++ app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/services/UserService.java
@@ -9,25 +9,28 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.function.Consumer;
+
 import edu.northeastern.cs5520_mobileappdev_team19.models.User;
 import edu.northeastern.cs5520_mobileappdev_team19.utils.UserViewAdapter;
 
 public class UserService {
-    private final UserViewAdapter userViewAdapter;
     private final DatabaseReference database;
     private static final String USERS = "users";
 
-    public UserService(UserViewAdapter userViewAdapter) {
-        this.userViewAdapter = userViewAdapter;
+    public UserService() {
         database = FirebaseDatabase.getInstance().getReference();
-        fetchUsers();
     }
 
-    public void fetchUsers() {
+    public void fetchUsers(UserViewAdapter userViewAdapter, User loggedInUser) {
         database.child(USERS).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                userViewAdapter.addUser(snapshot.getValue(User.class));
+                User user = snapshot.getValue(User.class);
+                if (user == null || user.getId().equals(loggedInUser.getId())) {
+                    return;
+                }
+                userViewAdapter.addUser(user);
             }
 
             @Override
@@ -53,6 +56,16 @@ public class UserService {
     }
 
     public void registerUser(User user) {
-        database.child(USERS).child(user.getUsername()).setValue(user);
+        database.child(USERS).child(user.getId()).setValue(user);
+    }
+
+    public void getUser(String id, Consumer<User> onSuccess, Consumer<Exception> onError) {
+        database.child(USERS).child(id).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                onSuccess.accept(task.getResult().getValue(User.class));
+            } else {
+                onError.accept(task.getException());
+            }
+        });
     }
 }
