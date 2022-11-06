2bb8d5149a746b0e52a8613a87cc93b30dd69bdd
sghosalkar <shubhamghosalkar.sg@gmail.com>
2022-11-06 13:24:09 -0800

    Fixed sticker received list issues

--- app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/utils/StickerReceivedAdapter.java
+++ app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/utils/StickerReceivedAdapter.java
@@ -10,30 +10,33 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.northeastern.cs5520_mobileappdev_team19.R;
 import edu.northeastern.cs5520_mobileappdev_team19.models.Message;
+import edu.northeastern.cs5520_mobileappdev_team19.services.UserService;
 
 public class StickerReceivedAdapter extends RecyclerView.Adapter<StickerReceivedViewHolder> {
     private final List<Message> messages;
     private final Context context;
-    private final String currentUserId;
+    private final UserService userService;
 
-    public StickerReceivedAdapter(Context context, String currentUserId) {
+    public StickerReceivedAdapter(Context context) {
         this.messages = new ArrayList<>();
         this.context = context;
-        this.currentUserId = currentUserId;
+        this.userService = new UserService();
     }
 
     @NonNull
     @Override
     public StickerReceivedViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new StickerReceivedViewHolder(LayoutInflater.from(context).inflate(viewType, parent, false));
+        return new StickerReceivedViewHolder(LayoutInflater.from(context).inflate(R.layout.sticker_received_item, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull StickerReceivedViewHolder holder, int position) {
         Message message = messages.get(position);
         holder.sticker.setImageResource(message.getStickerId());
-        holder.receivedFrom.setText(message.getRecipientId());
+        userService.getUser(message.getSenderId(), (user) -> holder.receivedFrom.setText(user.getUsername()), (error) -> {});
+
         holder.timestamp.setText(message.getTimestampAsString());
     }
 
@@ -42,5 +45,9 @@ public class StickerReceivedAdapter extends RecyclerView.Adapter<StickerReceived
         return messages.size();
     }
 
-
+    public void setMessages(List<Message> messages) {
+        this.messages.clear();
+        this.messages.addAll(messages);
+        this.notifyDataSetChanged();
+    }
 }
