f47861b0d13d25875df2be21717477c5d1aca078
Suhaani Agarwal <64849382+agarwalsuhaani@users.noreply.github.com>
2022-11-06 22:52:29 -0500

    Update StickerSentActivity.java

--- app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/StickerSentActivity.java
+++ app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/StickerSentActivity.java
@@ -23,6 +23,7 @@ public class StickerSentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sticker_sent);
+        this.senderId = getIntent().getStringExtra(SENDER_ID);
 
         stickerSentRecyclerView = findViewById(R.id.rv_stickerSent);
 
