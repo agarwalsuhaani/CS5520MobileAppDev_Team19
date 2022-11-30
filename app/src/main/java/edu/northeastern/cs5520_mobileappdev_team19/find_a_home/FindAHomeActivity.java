e21233e265cf68eab336707cb8dd9ce754fd802c
sghosalkar <shubhamghosalkar.sg@gmail.com>
2022-11-29 23:11:52 -0800

    Code reformatting

--- app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/find_a_home/FindAHomeActivity.java
+++ app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/find_a_home/FindAHomeActivity.java
@@ -8,22 +8,18 @@ import android.os.Bundle;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 
 import edu.northeastern.cs5520_mobileappdev_team19.R;
 
 public class FindAHomeActivity extends AppCompatActivity {
 
     private FirebaseUser user;
-    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
-            new FirebaseAuthUIActivityResultContract(),
-            result -> onSignInResult(result)
-    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,19 +28,29 @@ public class FindAHomeActivity extends AppCompatActivity {
 
         user = FirebaseAuth.getInstance().getCurrentUser();
         if (user == null) {
-            List<AuthUI.IdpConfig> providers = Collections.singletonList(new AuthUI.IdpConfig.EmailBuilder().build());
-            Intent signInIntent = AuthUI.getInstance()
-                    .createSignInIntentBuilder()
-                    .setAvailableProviders(providers)
-                    .build();
-            signInLauncher.launch(signInIntent);
+            requestSignIn(user -> {
+                this.user = user;
+                // TODO : Sign-in successful. Perform next steps...
+            });
         }
     }
 
-    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
-        if (result.getResultCode() == RESULT_OK) {
-            user = FirebaseAuth.getInstance().getCurrentUser();
-            // TODO : Sign-in successful. Perform next steps...
-        }
+    private void requestSignIn(Consumer<FirebaseUser> callback) {
+        ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+                new FirebaseAuthUIActivityResultContract(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        user = FirebaseAuth.getInstance().getCurrentUser();
+                        callback.accept(user);
+                    }
+                }
+        );
+
+        List<AuthUI.IdpConfig> providers = Collections.singletonList(new AuthUI.IdpConfig.EmailBuilder().build());
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build();
+        signInLauncher.launch(signInIntent);
     }
 }
