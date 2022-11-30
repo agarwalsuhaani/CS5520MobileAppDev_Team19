b65f8c8a8d6104188dc03c712ea468a2bf1c4e11
sghosalkar <shubhamghosalkar.sg@gmail.com>
2022-11-29 20:15:46 -0800

    Updated server url

--- app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/find_a_home/common/Constants.java
+++ app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/find_a_home/common/Constants.java
@@ -1,5 +1,5 @@
 package edu.northeastern.cs5520_mobileappdev_team19.find_a_home.common;
 
 public final class Constants {
-    public static final String SERVER_BASE_URL = "https://localhost:3000/";
+    public static final String SERVER_BASE_URL = "https://find-a-home-backend.fly.dev";
 }
