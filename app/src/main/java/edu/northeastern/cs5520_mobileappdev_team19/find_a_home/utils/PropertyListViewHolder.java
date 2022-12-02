63de106b5e23c6d098f07f0b92d41587c91f714a
Suhaani Agarwal <64849382+agarwalsuhaani@users.noreply.github.com>
2022-12-01 23:58:55 -0500

    Property List ViewHolder code added

--- app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/find_a_home/utils/PropertyListViewHolder.java
+++ app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/find_a_home/utils/PropertyListViewHolder.java
@@ -1,12 +1,23 @@
 package edu.northeastern.cs5520_mobileappdev_team19.find_a_home.utils;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import edu.northeastern.cs5520_mobileappdev_team19.R;
+
 public class PropertyListViewHolder extends RecyclerView.ViewHolder {
+    private ImageView iv_propertyImage;
+    private TextView tv_propertyName;
+    private TextView tv_propertyAddress;
+
     public PropertyListViewHolder(@NonNull View itemView) {
         super(itemView);
+        iv_propertyImage = itemView.findViewById(R.id.iv_propertyList);
+        tv_propertyName = itemView.findViewById(R.id.tv_propertyName);
+        tv_propertyAddress = itemView.findViewById(R.id.tv_propertyAddress);
     }
 }
