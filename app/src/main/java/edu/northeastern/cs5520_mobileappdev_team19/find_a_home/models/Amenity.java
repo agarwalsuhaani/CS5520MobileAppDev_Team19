c96fa1c74dcdfc2fb97856fb3606cb1150334f66
sghosalkar <shubhamghosalkar.sg@gmail.com>
2022-12-05 19:01:14 -0800

    Added amenity model

--- app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/find_a_home/models/Amenity.java
+++ app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/find_a_home/models/Amenity.java
@@ -1,6 +1,23 @@
 package edu.northeastern.cs5520_mobileappdev_team19.find_a_home.models;
 
-public interface Amenity {
-    int getId();
-    String getName();
+import com.google.gson.annotations.SerializedName;
+
+public class Amenity {
+    @SerializedName("id")
+    private String id;
+
+    @SerializedName("name")
+    private final String name;
+
+    public Amenity(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
