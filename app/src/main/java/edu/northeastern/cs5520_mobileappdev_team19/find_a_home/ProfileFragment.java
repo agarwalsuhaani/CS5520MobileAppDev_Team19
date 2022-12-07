6703628c00fb4b9654aa1e5d3bb2a6fdfb95f934
Suhaani Agarwal <64849382+agarwalsuhaani@users.noreply.github.com>
2022-12-06 22:21:21 -0500

    Update ProfileFragment.java

--- app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/find_a_home/ProfileFragment.java
+++ app/src/main/java/edu/northeastern/cs5520_mobileappdev_team19/find_a_home/ProfileFragment.java
@@ -23,28 +23,6 @@ import edu.northeastern.cs5520_mobileappdev_team19.R;
  */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-//    public ProfileFragment() {
-//        // Required empty public constructor
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
